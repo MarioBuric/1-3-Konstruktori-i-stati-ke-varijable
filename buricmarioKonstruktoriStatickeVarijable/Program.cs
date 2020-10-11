d8623a68916310621d252bedffa51f2579939253
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-10-11 21:51:21 +0200

    Add files via upload

---
+++ buricmarioKonstruktoriStatickeVarijable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buricmarioKonstruktoriStatickeVarijable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa X = new PrvaKlasa("Andrej", "Kramarić");
+            PrvaKlasa Y = new PrvaKlasa("Nikola", "Vlašić");
+            PrvaKlasa Z = new PrvaKlasa();
+            
+            Console.WriteLine(X+ "\n" + Y + "\n" + Z);
+            Console.ReadKey();
+        }
+    }
+}
